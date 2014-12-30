f8ed88c1563d3f4b451fdd9ccb4ee8df74dce65d
fedassa <fedassa@gmail.com>
2014-12-30 17:44:44 +0100

    first commit

--- src/msdImgPyramid.cpp
+++ src/msdImgPyramid.cpp
@@ -1,4 +1,22 @@
 
+//This file is part of the MSD-Detector project.
+//
+//The MSD-Detector is free software : you can redistribute it and / or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//The MSD-Detector is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with Foobar.If not, see <http://www.gnu.org/licenses/>.
+// 
+// AUTHOR: Federico Tombari (fedassa@gmail.com)
+// University of Bologna, Open Perception
+
 #include "msdImgPyramid.h"
 
 ImagePyramid::ImagePyramid(const cv::Mat & im, const int nLevels, const float scaleFactor)
